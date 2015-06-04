75702d3692b3edba9bc9ab3d8ade0dc890ea8516
Andrew Benz <andorbal@gmail.com>
2015-06-04 18:07:22 -0500

    Initial commit

---
+++ chapter5/www/js/app.js
@@ -0,0 +1,70 @@
+angular.module('App', ['ionic', 'highcharts-ng'])
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('tabs', {
+      url: '/tabs',
+      abstract: true,
+      templateUrl: 'views/tabs/tabs.html'
+    })
+    .state('tabs.rates', {
+    	url: '/rates',
+    	views: {
+    		'rates-tab': {
+    			templateUrl: 'views/rates/rates.html',
+    			controller: 'RatesController'
+    		}
+    	}
+    })
+    .state('tabs.history', {
+    	url: '/history?currency',
+    	views: {
+    		'history-tab': {
+    			templateUrl: 'views/history/history.html',
+                controller: 'HistoryController'
+    		}
+    	}
+    })
+    .state('tabs.currencies', {
+    	url: '/currencies',
+    	views: {
+    		'currencies-tab': {
+    			templateUrl: 'views/currencies/currencies.html',
+                controller: 'CurrenciesController'
+    		}
+    	}
+    })
+    .state('tabs.detail', {
+        url: '/detail/:currency',
+        views: {
+            'rates-tab': {
+                templateUrl: 'views/detail/detail.html',
+                controller: 'DetailController'
+            }
+        }
+    });
+
+    $urlRouterProvider.otherwise('/tabs/rates');
+})
+.factory('Currencies', function() {
+	return [
+		{ code: 'AUD', text: 'Australian Dollar', selected: true },
+		{ code: 'BRL', text: 'Brazilian Real', selected: false },
+	    { code: 'CAD', text: 'Canadian Dollar', selected: true },
+	    { code: 'CHF', text: 'Swiss Franc', selected: false },
+	    { code: 'CNY', text: 'Chinese Yuan', selected: true},
+	    { code: 'EUR', text: 'Euro', selected: true },
+	    { code: 'GBP', text: 'British Pound Sterling', selected: true },
+		{ code: 'IDR', text: 'Indonesian Rupiah', selected: false },
+	    { code: 'ILS', text: 'Israeli New Sheqel', selected: false },
+	    { code: 'MXN', text: 'Mexican Peso', selected: true },
+	    { code: 'NOK', text: 'Norwegian Krone', selected: false },
+	    { code: 'NZD', text: 'New Zealand Dollar', selected: false },
+	    { code: 'PLN', text: 'Polish Zloty', selected: false },
+	    { code: 'RON', text: 'Romanian Leu', selected: false },
+	    { code: 'RUB', text: 'Russian Ruble', selected: true },
+	    { code: 'SEK', text: 'Swedish Krona', selected: false },
+	    { code: 'SGD', text: 'Singapore Dollar', selected: false },
+	    { code: 'USD', text: 'United States Dollar', selected: true },
+	    { code: 'ZAR', text: 'South African Rand', selected: false }
+	];
+});
---
+++ chapter5/www/views/rates/rates.js
@@ -0,0 +1,35 @@
+angular.module('App')
+.controller('RatesController', function($scope, $http, 
+		$ionicPopover, Currencies) {
+	$scope.currencies = Currencies;
+
+	$ionicPopover.fromTemplateUrl('views/rates/help-popover.html', {
+		scope: $scope
+	})
+	.then(function(popover) {
+		$scope.popover = popover;
+	});
+
+	$scope.openHelp = function($event) {
+		$scope.popover.show($event);
+	};
+
+	$scope.$on('$destroy', function() {
+		$scope.popover.remove();
+	});
+
+	$scope.load = function() {
+		$http.get('https://api.bitcoinaverage.com/ticker/all')
+			.success(function(tickers) {
+				angular.forEach($scope.currencies, function(currency) {
+					currency.ticker = tickers[currency.code];
+					currency.ticker.timestamp = new Date(currency.ticker.timestamp);
+				});
+			})
+			.finally(function() {
+				$scope.$broadcast('scroll.refreshComplete');
+			});
+	};
+
+	$scope.load();
+});
